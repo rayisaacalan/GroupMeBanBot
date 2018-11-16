4c815b2f23dc5e8d3f44de3a2bef1ec91dd39e9e
rayisaacalan <44532725+rayisaacalan@users.noreply.github.com>
2018-11-16 15:42:30 -0600

    Simplifying, let's just get something to work

--- app.py
+++ app.py
@@ -12,62 +12,22 @@ app = Flask(__name__)
 TOKEN = os.getenv('GM_TOKEN')
 GMClient = Client.from_token(TOKEN)
 
-
 @app.route('/', methods=['POST'])
-def post():
+def webhook():
     data = request.get_json()
-    log('Recieved {}'.format(data))
-    groupID = data['group_id']
-    bot = get_bot(groupID, name)
-    group = get_group(groupID)
-    # We don't want to reply to ourselves!
-    if data['name'] != bot.name:
-       msg = data['text']
-    if '@all' in msg:
-        at_all(bot, group)
-    if 'TypeThis' in msg:
-        bot.post("PostThis")
 
+    if data['name'] != 'Testy McTestson':
+        msg = '{}, you just said "{}". Your user id is {} and your membership id is {}.'.format(data['name'], data['text'], data['user_id'], data['id'])
+        print(data, flush = True)
+        send_message(msg)
     return "ok", 200
 
-def at_all(bot, group):
-    members = group.members()
-    user_ids = []
-    loci = []
-    text = ""
-    pnt = 0
+def send_message(msg):
+    url = 'https://api.groupme.com/v3/bots/post'
 
-    for m in members:
-        curm = m.identification()
-        id = curm["user_id"]
-        name = "@" + curm["nickname"] + " "
-
-        user_ids.append(id)
-
-        n = [pnt, len(name)]
-        loci.append(n) 
-        pnt += len(name)
-
-    text += name
-
-    mention = {}
-    mention["type"] = "mentions"
-    mention["user_ids"] = user_ids
-    mention["loci"] = loci
-
-    bot.post(text, mention)
-
-def get_bot(groupID, name):
-    for b in Client.bots.list():
-        if b.group_id == groupID:
-            if b.username == name:
-                return b
-
-def get_group(groupID):
-    for g in Client.groups.list():
-        if g.group_id == groupID:
-            return g
-
-def log(msg):
-    print(str(msg))
-    sys.stdout.flush()
+    data = {
+            'bot_id' : os.getenv('GROUPME_TESTBOT_ID'),
+            'text'   : msg,
+    }
+    request = Request(url, urlencode(data).encode())
+    json = urlopen(request).read().decode()
