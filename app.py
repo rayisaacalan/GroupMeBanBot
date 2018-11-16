e9123d38083f4c0429e9124ea845ce673997b90f
rayisaacalan <44532725+rayisaacalan@users.noreply.github.com>
2018-11-16 16:47:15 -0600

    Adding kick functionality (hopefully)

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 import groupy
+import random
 from groupy.client import Client, attachments
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
@@ -11,23 +12,40 @@ app = Flask(__name__)
 
 TOKEN = os.getenv('GM_TOKEN')
 GMClient = Client.from_token(TOKEN)
+BANNABLE_USER = os.getenv('BAN_USER_ID') #user_id
+GROUP_ID = os.getenv('GROUP_ID')
+BOT_ID = os.getenv('GROUPME_TESTBOT_ID')
 
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
 
-    if data['name'] != 'Testy McTestson':
-        msg = '{}, you just said "{}". Your user id is {} and your membership id is {}.'.format(data['name'], data['text'], data['user_id'], data['id'])
-        print(data, flush = True)
-        send_message(msg)
+    if data['id'] == BANNABLE_USER:
+        if not random.randint(0,1):
+            print(data, flush = True)
+            remove_user(data['id'])
     return "ok", 200
 
 def send_message(msg):
     url = 'https://api.groupme.com/v3/bots/post'
 
     data = {
-            'bot_id' : os.getenv('GROUPME_TESTBOT_ID'),
+            'bot_id' : BOT_ID,
             'text'   : msg,
     }
     request = Request(url, urlencode(data).encode())
     json = urlopen(request).read().decode()
+    print("RESPONSE:")
+    print(json, flush = True)
+
+def remove_user(memID):
+    send_message('That was not very cash money of you.')
+    url = 'https://api.groupme.com/v3/groups/' + GROUP_ID +'/members/' + memID +'/remove'
+
+    request = Request(url)
+    json = urlopen(request).read().decode()
+    print("RESPONSE:")
+    print(json, flush = True)
+
+
+
